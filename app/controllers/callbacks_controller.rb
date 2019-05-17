5c908caa67122f2241f3a94d13b236f5250c3a0d
Emilio <eivelez@miuandes.cl>
2019-05-17 02:10:40 -0400

    finally found the problem, i dropped de DB and it worked....

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,5 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
-      # You need to implement the method below in your model (e.g. app/models/user.rb)
+	def google_oauth2
       @user = User.from_omniauth(request.env['omniauth.auth'])
 
       if @user.persisted?
@@ -8,7 +7,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
         sign_in_and_redirect @user, event: :authentication
       else
         session['devise.google_data'] = request.env['omniauth.auth'].except(:extra) # Removing extra as it can overflow some session stores
-        redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+        redirect_to new_user_registration_url, notice: @user.errors.full_messages.join("\n")
       end
-  end
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -53,16 +53,23 @@ ActiveRecord::Schema.define(version: 2019_05_16_221032) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "last_name"
-    t.string "email", default: "", null: false
     t.string "phone"
     t.string "password"
     t.string "address"
+    t.string "provider"
+    t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
