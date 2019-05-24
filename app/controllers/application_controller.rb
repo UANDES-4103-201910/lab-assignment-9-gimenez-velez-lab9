4fe037dd2aab2314771d47659f1304d904ad635b
Emilio <eivelez@miuandes.cl>
2019-05-24 00:09:37 -0400

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  layout proc { google_logged_in ? "glogged_in" : "application" }
   before_action :authenticate_user!
 
 
