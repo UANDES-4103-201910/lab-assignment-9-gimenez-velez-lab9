41c2d586097a4506946d7b6b0f100a5b08cca3d8
Emilio <eivelez@miuandes.cl>
2019-05-23 23:57:09 -0400

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,16 +11,12 @@ class ApplicationController < ActionController::Base
 
   def current_user
 	 @current_user ||= session[:current_user_id] && User.find_by_id(session[:current_user_id])
-
-  end
+ end
 
   def is_user_logged_in?
-	#complete this method
-
     logged_in = current_user
-	 if current_user then true else redirect_to root_path end
+    if current_user then true else redirect_to root_path end
   end
-
   def google_logged_in
     if session["warden.user.user.key"] then true else false end
   end
